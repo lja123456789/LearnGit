a77c8763d8034a259ca2b692ca032315593f5781
lja_global <ljg_global@learn.com>
2022-06-06 20:46:42 +0800

    "成员二修改了同一个文件产生冲突"

--- src/main/java/com/lja/learngit/Happy.java
+++ src/main/java/com/lja/learngit/Happy.java
@@ -4,5 +4,8 @@ public class Happy {
 
 	public static void main(String args[]) {
 		System.out.println("test ....");
+		System.out.println("成员二做了修改，然后add 并提交到本地"
+				+ "然后push时就会出现冲突,需要先pull,然后解决冲突，"
+				+ "然后在add 、commit、push");
 	}
 }
