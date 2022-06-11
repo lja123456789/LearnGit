75f917e0ff3ad1bb337cc3806fdafb3e67f5da7c
lja_global <ljg_global@learn.com>
2022-06-11 18:26:11 +0800

    "模拟利用新分支开发新任务"

--- src/main/java/com/lja/learngit/Happy.java
+++ src/main/java/com/lja/learngit/Happy.java
@@ -10,5 +10,9 @@ public class Happy {
 		//同时保留两部分内容
 		System.out.println("成员一做修改，然后add，并提交到"
 				+ "本地库,然后push到远程库。");
+		
+		
+		//成员在feature上开发新任务
+		System.out.println("正在开发新的任务......");
 	}
 }
